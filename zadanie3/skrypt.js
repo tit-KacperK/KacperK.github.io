d6ecfed2373b8d125a9d086ff8a2557da69bd14c
tit-KacperK <64531217+tit-KacperK@users.noreply.github.com>
2020-05-26 16:35:27 +0200

    Add files via upload

---
+++ zadanie3/skrypt.js
@@ -0,0 +1,76 @@
+function toggle(x, y) {
+	document.getElementById(x).classList.add('hidden');
+	document.getElementById(y).classList.add('active');
+}
+function zmien1()
+{
+	document.getElementById('foto1').src="zdj1.png";
+}
+function zmien2()
+{
+	document.getElementById('foto1').src="zdj2.png";
+}
+function zmien3()
+{
+	document.getElementById('foto2').src="zdj3.png";
+}
+function zmien4()
+{
+	document.getElementById('foto2').src="zdj4.png";
+}
+function zmien5()
+{
+	document.getElementById('foto3').src="zdj5.png";
+}
+function zmien6()
+{
+	document.getElementById('foto3').src="zdj6.png";
+}
+function zmien7()
+{
+	document.getElementById('foto4').src="zdj7.png";
+}
+function zmien8()
+{
+	document.getElementById('foto4').src="zdj8.png";
+}
+function zmien9()
+{
+	document.getElementById('foto5').src="zdj9.png";
+}
+function zmien10()
+{
+	document.getElementById('foto5').src="zdj10.png";
+}
+function zmien11()
+{
+	document.getElementById('foto6').src="zdj11.png";
+}
+function zmien12()
+{
+	document.getElementById('foto6').src="zdj12.png";
+}
+function zmien13()
+{
+	document.getElementById('foto7').src="zdj13.png";
+}
+function zmien14()
+{
+	document.getElementById('foto7').src="zdj14.png";
+}
+function zmien15()
+{
+	document.getElementById('foto8').src="zdj15.png";
+}
+function zmien16()
+{
+	document.getElementById('foto8').src="zdj16.png";
+}
+function zmien17()
+{
+	document.getElementById('foto9').src="zdj17.png";
+}
+function zmien18()
+{
+	document.getElementById('foto9').src="zdj18.png";
+}
